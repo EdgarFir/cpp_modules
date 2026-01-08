d1e6d22c8a402d32ec4992935be72398e5b47632
EdgarFir <edreal10@outlook.pt>
2026-01-08 17:14:02 +0000

    Created ex02 files

---
+++ 05/ex02/AForm.cpp
@@ -0,0 +1,144 @@
+#include "AForm.hpp"
+
+AForm::AForm() : _is_signed(false), _sign_grade(150), _exec_grade(150) {}
+
+AForm::AForm(const std::string &name, int sign_grade, int _exec_grade) 
+    : _name(name), _is_signed(false), _sign_grade(sign_grade), _exec_grade(_exec_grade) {
+    if (_sign_grade < GRADE_MAX)
+        throw GradeTooHighException("Error: Form sign grade too high, max: 1\n");
+    if (_sign_grade > GRADE_MIN)
+        throw GradeTooLowException("Error: Form sign grade too low, min: 150\n");
+    if (_exec_grade < GRADE_MAX)
+        throw GradeTooHighException("Error: Form sign grade too high, max: 1\n");
+    if (_exec_grade > GRADE_MIN)
+        throw GradeTooLowException("Error: Form sign grade too low, min: 150\n");
+}
+
+AForm::AForm(const AForm &src) 
+    : _name(src._name), _is_signed(src._is_signed), _sign_grade(src._sign_grade), _exec_grade(src._exec_grade) {
+}
+
+AForm::~AForm() {}
+
+/**
+ * @brief Equal operator overload. 
+ * If src not equal to actual object, assign src non const atributtes.
+ *
+ * @param src 
+ * @return Return this
+ */
+AForm &AForm::operator=(const AForm &src) {
+    
+    if (this != &src) {
+        this->_is_signed = src._is_signed;
+    }
+    return *this;
+}
+
+/**
+ * @brief AForm name getter.
+ * 
+ * @return AForm name
+ */
+std::string AForm::getName() const {
+    return _name;
+}
+
+/**
+ * @brief AForm sign grade getter.
+ * 
+ * @return AForm sign grade.
+ */
+int         AForm::getSignGrade() const {
+    return _sign_grade;
+}
+
+/**
+ * @brief AForm exec grade getter.
+ * 
+ * @return AForm exec grade.
+ */
+int         AForm::getExecGrade() const {
+    return _exec_grade;
+}
+
+bool        AForm::getIsSigned() const {
+    return _is_signed;
+}
+
+AForm::GradeTooHighException::GradeTooHighException(const std::string & msg) throw() {
+    _msg = msg;
+}
+
+AForm::GradeTooHighException::GradeTooHighException::~GradeTooHighException() throw() {}
+
+/**
+ * @brief Overrides std::exception 'what' method.
+ * 
+ * @return Exception message.
+ */
+const char * AForm::GradeTooHighException::GradeTooHighException::what() const throw() {
+    return _msg.c_str();
+}
+
+AForm::GradeTooLowException::GradeTooLowException(const std::string & msg) throw() {
+    _msg = msg;
+}
+
+AForm::GradeTooLowException::GradeTooLowException::~GradeTooLowException() throw() {}
+
+/**
+ * @brief Overrides std::exception 'what' method.
+ * 
+ * @return Exception message.
+ */
+const char * AForm::GradeTooLowException::GradeTooLowException::what() const throw() {
+    return _msg.c_str();
+}
+
+AForm::AlreadySignedException::AlreadySignedException(const std::string & msg) throw() {
+    _msg = msg;
+}
+
+AForm::AlreadySignedException::AlreadySignedException::~AlreadySignedException() throw() {}
+
+/**
+ * @brief Overrides std::exception 'what' method.
+ * 
+ * @return Exception message.
+ */
+const char * AForm::AlreadySignedException::AlreadySignedException::what() const throw() {
+    return _msg.c_str();
+}
+
+/**
+ * @brief Ensure Bureaucrat grade is not lower than AForm sign grade,
+ * else throw exception.
+ * 
+ * @param b Bureaucrat object.
+ */
+void        AForm::beSigned(const Bureaucrat &b) {
+    if (_is_signed)
+        throw AlreadySignedException("AForm is already signed\n");
+    if (b.getGrade() > _sign_grade)
+        throw GradeTooLowException("bureaucrat grade not enough to sign AForm\n");
+    _is_signed = true;
+}
+
+/**
+ * @brief Insertion operator overload.
+ * 
+ * @param os Ostream object.
+ * @param obj Form object.
+ * 
+ * @return Ossteam object reference.
+ */
+std::ostream &operator<<(std::ostream & os, const AForm & obj) {
+    os << obj.getName() << ", "
+       << "signed: " << obj.getSignGrade()
+       << ", sign grade: " << obj.getSignGrade()
+       << ", exec grade: " << obj.getExecGrade()
+       << std::endl;
+
+    return os;
+}
---
+++ 05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,25 @@
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() {
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string & target) {
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm & src) {
+
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {
+
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm & src) {
+
+}
+
+void        ShrubberyCreationForm::execute(Bureaucrat const & executor) const {
+    
+}
